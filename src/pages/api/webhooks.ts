cfde4267b35fd83bcae5ca30a68a3b1787a77972
DevsJ <douglasmb87@gmail.com>
2022-07-04 14:55:22 -0300

    add webHooks

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,58 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+import Stripe from "stripe";
+import { StripeApi } from "../../services/stripe";
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk, "utf8") : chunk);
+  }
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+const relevantEvents = new Set(["checkout.session.completed"]);
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const buf = await buffer(req);
+    const secret = req.headers["stripe-signature"] || "";
+    let event: Stripe.Event;
+
+    try {
+      const stripeApi = new StripeApi();
+      event = stripeApi.stripe.webhooks.constructEvent(
+        buf,
+        secret,
+        process.env.STRIPE_WEBHOOK_SECRET || ""
+      );
+    } catch (err) {
+      console.log(err);
+      return res.status(400).send(`WebHook Error: ${err}`);
+    }
+
+    const { type } = event;
+    if (relevantEvents.has(type)) {
+      try {
+        switch (type) {
+          case "checkout.session.completed":
+            break;
+          default:
+            throw new Error(`Unknown event type: ${type}`);
+        }
+      } catch (error) {
+        return res.json({error:'webhook error'});
+      }
+    }
+
+    res.status(200).json({ received: true });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method not allowed");
+  }
+};
